581350a04eb0274319f1b0d67f6d3cdfb50748f4
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-12-22 12:24:22 -0800

    adding basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 
 import HomePage from './components/pages/homepage/homepage.component.jsx';
+
+const HatsPage = () =>{
+return(
+  <div>
+    <h1>Hats Page</h1>
+  </div>)
+}
+
 
 function App() {
   return (
     <div >
-      <HomePage></HomePage>
+   
+      <Routes>
+          <Route path="/" element={<HomePage/>}> </Route>
+          <Route path="/hats" element={<HatsPage/>}> </Route>
+          <Route path="*" element={<HomePage/>} /> 
+          </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
