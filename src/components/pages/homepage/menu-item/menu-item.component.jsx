e4faa2627bbf633f24946567625addc62562c9e0
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2021-12-22 11:37:30 -0800

    added white background to menu items styles

--- src/components/pages/homepage/menu-item/menu-item.component.jsx
+++ src/components/pages/homepage/menu-item/menu-item.component.jsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
+
+import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style ={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-      
-      className= {`${size} menu-item`}>
-        <div className='content'>
-          <div className='title'> { title }</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-
-
-)
-
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
 
 export default MenuItem;
-
